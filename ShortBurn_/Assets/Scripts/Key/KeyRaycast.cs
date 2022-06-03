3787e72c0cc11c0baf90bbbe4cac1e82253e9757
OzoOnne <200486@student.glu.nl>
2022-06-03 12:57:23 +0200

    scene setup and scripts added

---
+++ ShortBurn_/Assets/Scripts/Key/KeyRaycast.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyRaycast : MonoBehaviour
+{
+    [SerializeField] private int _rayLenght = 5;
+    [SerializeField] private LayerMask _layerMaskInteract;
+    [SerializeField] private string _excluseLayerNam = null;
+    
+    
+}
