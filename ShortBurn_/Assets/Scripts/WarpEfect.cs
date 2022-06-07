39f8a2a33dcc87c03c5f49cfaae57cd3892081d8
Sjoerd De Koning <78423327+SjoerdDeKoning@users.noreply.github.com>
2022-06-07 11:18:36 +0200

    warp effect done i hope i pray to god

--- ShortBurn_/Assets/Scripts/WarpEfect.cs
+++ ShortBurn_/Assets/Scripts/WarpEfect.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cinemachine;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class WarpEfect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CinemachineVirtualCamera CVCamera;
+    [SerializeField] private AnimationCurve Curve;
+    [SerializeField] private float speed = 1;
+    public bool DoWarp = false; 
+    
+    private float _current;
+    private void Start()
     {
-        
+        CVCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        if (CVCamera == null)
+        {
+            Debug.LogError(this.name +" can not find CinemachineVirtualCamera");
+        }
+    }
+    private void Update()
+    {
+        if (DoWarp)
+        {
+            WarpEffect();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void WarpEffect()
     {
+        if (_current >= 1)
+        {
+            DoWarp = false;
+            _current = 0;
+        }
         
+        _current = Mathf.MoveTowards(_current, 1, speed * Time.deltaTime);
+        CVCamera.m_Lens.FieldOfView = math.lerp(60f, 0f,Curve.Evaluate(_current));
     }
 }
