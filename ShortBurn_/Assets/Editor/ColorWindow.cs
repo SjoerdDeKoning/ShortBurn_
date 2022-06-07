b5506068b1f9aa4b1e307138c198d71950650bc6
OzoOnne <200486@student.glu.nl>
2022-06-07 10:24:30 +0200

    ColorPicker added

---
+++ ShortBurn_/Assets/Editor/ColorWindow.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEditor;
+using UnityEngine;
+
+public class ColorWindow : EditorWindow
+{ 
+    Color color;
+
+    [MenuItem("Tools/ColorPicker")]
+    public static void ShowWindow()
+    {
+        GetWindow<ColorWindow>("ColorPicker");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Color for the selected object");
+
+        color = EditorGUILayout.ColorField("Color", color);
+
+        if (GUILayout.Button("Give it Color!"))
+        {
+            foreach (GameObject obj in Selection.gameObjects)
+            {
+                Renderer renderer = obj.GetComponent<Renderer>();
+                if (renderer != null)
+                {
+                    renderer.sharedMaterial.color = color;
+                }
+            }
+        }
+        
+        if (GUILayout.Button("Reset to white"))
+        {
+            foreach (GameObject obj in Selection.gameObjects)
+            {
+                Renderer renderer = obj.GetComponent<Renderer>();
+                if (renderer != null)
+                {
+                    renderer.sharedMaterial.color = Color.white;
+                }
+            }
+        }
+    }
+}
